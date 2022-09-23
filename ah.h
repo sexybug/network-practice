d70b860b9fc345d11dff3d6b28c32a3beaea2f5a
sexybug <hello.bug@outlook.com>
2022-09-23 09:55:51 +0800

    实现AH传输模式收发Bug修复

--- ah.h
+++ ah.h
@@ -1,24 +1,25 @@
 #include <linux/types.h>
 
-struct ip_auth_hdr {
+struct ip_auth_hdr
+{
     /* 下一个头 */
-	__u8  nexthdr;
+    __u8 nexthdr;
 
     /* 载荷长度 */
-	__u8  hdrlen;		/* This one is measured in 32 bit units! */
+    __u8 hdrlen; /* This one is measured in 32 bit units! */
 
     /* 保留 */
-	__be16 reserved;
+    __be16 reserved;
 
     /* 安全参数索引 */
-	__be32 spi;
+    __be32 spi;
 
     /* 序列号 */
-	__be32 seq_no;		/* Sequence number */
-    
+    __be32 seq_no; /* Sequence number */
+
     /*鉴别数据（变长的） */
     /* modified by sexybug to be compatible with c99 */
-	__u8  auth_data[];	/* Variable len but >=4. Mind the 64 bit alignment! */
+    __u8 auth_data[]; /* Variable len but >=4. Mind the 64 bit alignment! */
 };
 
 /* 网卡接口默认MTU=1500 */
