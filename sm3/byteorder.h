ba97034c52d396aef5fccfb7c951d63f51099fde
sexybug <hello.bug@outlook.com>
2022-10-09 16:08:58 +0800

    rename files

--- sm3/byteorder.h
+++ sm3/byteorder.h
@@ -1,6 +1,5 @@
-#ifndef ZCRYPT_BYTEORDER_H
-#define ZCRYPT_BYTEORDER_H
-
+#ifndef BYTEORDER_H
+#define BYTEORDER_H
 
 #ifdef CPU_BIGENDIAN
 
@@ -11,12 +10,11 @@
 
 #else
 
-#define cpu_to_be16(v) (((v)<< 8) | ((v)>>8))
-#define cpu_to_be32(v) (((v)>>24) | (((v)>>8)&0xff00) | (((v)<<8)&0xff0000) | ((v)<<24))
+#define cpu_to_be16(v) (((v) << 8) | ((v) >> 8))
+#define cpu_to_be32(v) (((v) >> 24) | (((v) >> 8) & 0xff00) | (((v) << 8) & 0xff0000) | ((v) << 24))
 #define be16_to_cpu(v) cpu_to_be16(v)
 #define be32_to_cpu(v) cpu_to_be32(v)
 
 #endif
 
 #endif
-
